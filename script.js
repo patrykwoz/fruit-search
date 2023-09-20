34eee65ee16d57796a1e9f8d9cded458abd9a91b
root <root@PATRYK-NFHTHTE0>
2023-09-19 22:28:29 -0400

    Completed basic setup and finished search function

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
-	let results = [];
-
-	// TODO
-
+	const lowerCaseFruit = fruit.map(word => word.toLowerCase());
+	const lowerCaseStr = str.toLowerCase();
+	const results = lowerCaseFruit.filter(fruit => fruit.includes(lowerCaseStr));
 	return results;
 }
 
+
+
 function searchHandler(e) {
 	// TODO
+	console.log(search(e.target.value));
+
+	
 }
 
 function showSuggestions(results, inputVal) {
@@ -24,5 +28,15 @@ function useSuggestion(e) {
 	// TODO
 }
 
-input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
+
+
+function debounce(func, delay) {
+    let debounceTimer;
+    return function(...args) {
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(() => func(...args), delay);
+    };
+}
+
+input.addEventListener('keypress', debounce(searchHandler,500));
+suggestions.addEventListener('click', useSuggestion); 
